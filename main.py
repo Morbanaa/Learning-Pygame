878b566a0434582b94f6a05b87f67c171c9cac69
Morbanaa <theodorerodd@gmail.com>
2026-05-01 17:44:15 -0500

    Added sections for main game loop and changed background fill

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Learning Pygame!")
 
+clock = pygame.time.Clock()
 running = True
-
 while running:
+    clock.tick(60)
+    # Input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # Update
+
+
+    # Draw
+    screen.fill((255,255,255))
+    pygame.display.update()
 pygame.quit()
 
