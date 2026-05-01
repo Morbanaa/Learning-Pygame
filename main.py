404c2db1d080b66b2ac931299aa4083f67c1134e
Morbanaa <theodorerodd@gmail.com>
2026-05-01 18:21:54 -0500

    Finished Practice

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ GAME_WIDTH = 1000
 GAME_HEIGHT = 1000
 
 class Player():
-    def __init__(self,xpos,ypos):
+    def __init__(self,xpos,ypos,speed):
         self.xpos = xpos
         self.ypos = ypos
+        self.speed = speed
 
 # Set up window
 pygame.init()
@@ -14,7 +15,7 @@ screen = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Learning Pygame!")
 
 # Create game objects
-player = Player(GAME_WIDTH//2,GAME_HEIGHT//2)
+player = Player(GAME_WIDTH//2,GAME_HEIGHT//2,5)
 clock = pygame.time.Clock()
 
 running = True
@@ -25,16 +26,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Update
-    keys = pygame.key.get_pressed()
 
+    #Controls WSAD
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        player.ypos -=2
+        player.ypos -= player.speed
     if keys[pygame.K_s]:
-        player.ypos +=2
+        player.ypos += player.speed
     if keys[pygame.K_a]:
-        player.xpos -=2
+        player.xpos -= player.speed
     if keys[pygame.K_d]:
-        player.xpos +=2
+        player.xpos += player.speed
 
     # Draw
     screen.fill((255,255,255))
