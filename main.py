10386e9fdee9880865b43804b37aecd11a85f78c
Morbanaa <theodorerodd@gmail.com>
2026-05-01 18:02:11 -0500

    Created player class with x and y pos and created Const vars for game set up

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 
-pygame.init()
+GAME_WIDTH = 1000
+GAME_HEIGHT = 1000
 
-screen = pygame.display.set_mode((800,600))
+class Player():
+    def __init__(self,xpos,ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+
+# Set up window
+pygame.init()
+screen = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Learning Pygame!")
 
+# Create game objects
+player = Player(GAME_WIDTH//2,GAME_HEIGHT//2)
 clock = pygame.time.Clock()
+
 running = True
 while running:
     clock.tick(60)
@@ -18,6 +29,7 @@ while running:
 
     # Draw
     screen.fill((255,255,255))
+    pygame.draw.circle(screen, (0, 255, 0), (player.xpos , player.ypos), 25)
     pygame.display.update()
 pygame.quit()
 
